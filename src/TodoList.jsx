3070bf97e52a33a91901bb538ed20d705345fcf7
Priyanshu rawat <priyanshurawat0311@gmail.com>
2026-02-10 21:42:45 +0530

    Day 5: Added toggle and delete features to TodoList

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -9,20 +9,51 @@ export default function TodoList() {
     ]);
     const [inputText, setInputText] = useState("");
 
+    const handleAdd= ()=> {
+       if(inputText.trim() ==="")
+        return;
+        const newTodo ={
+        id: Date.now(),
+        text: inputText,
+        completed:false,
+        };
+        setTodos([...todos, newTodo]);
+        setInputText("");
+    } 
+
+    const handleToggle =(id)=>{
+       setTodos(
+        todos.map((todo) =>
+         todo.id=== id ? {...todo, completed: !todo.completed} :todo
+       )
+       );
+       }
+
+    const handleDelete=(id)=>{
+         setTodos(todos.filter((todo) =>
+                todo.id !== id));
+    }
+
     return (
         <div>
             <h3>My Todo List</h3>
+            <div>
+                <input type="text" 
+                value={inputText} 
+                onChange={(e) =>setInputText(e.target.value)}
+                placeholder="Enter a todo..."></input>
+                <button onClick={handleAdd}>Add</button>
+            </div>
             {todos.map((todo) => (
                 <div key={todo.id}>
                 <input type="checkbox" checked={todo.completed}
-                onChange={()=>{}}></input>
+                onChange={()=>handleToggle(todo.id)}></input>
                 <span  style ={{textDecoration: todo.completed ? "line-through":"none"}}>
                     {todo.text}
                 </span>
-            <button onClick={() =>{}}>Delete</button>
+            <button onClick={()=>handleDelete(todo.id)}>Delete</button>
             </div>
             ))}
-           
         </div>
     )
 }
